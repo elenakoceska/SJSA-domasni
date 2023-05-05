a8d1b62b32921de7ae1b20d06b67ec4b2304d72e
elenakoceska <127784516+elenakoceska@users.noreply.github.com>
2023-05-05 19:37:12 +0200

    Add files via upload

---
+++ domasna C2/script.js
@@ -0,0 +1,59 @@
+var name = "Elena";
+console.log(name);
+
+
+//zadaca br.1
+
+var N = 2;
+var Z = "Milan";
+var Y = "Skopje";
+var X = "sales manager";
+
+console.log('You will be ' + X + ' in ' + Y + ' , and married to ' + Z + ' with ' + N + ' kids.');
+
+
+//zadaca br.2
+
+var date = 1984;
+var date2 = 2024;
+var date3 = 2024;
+var result1 = date2 - date;
+var result2 = date3 - date;
+
+console.log(`I will be either ${result1} or ${result2} in ${date2}. `)
+
+
+// zadaca br.3
+
+var my_age = 39;
+var age = 100;
+var day = 2;
+var result = age - my_age;
+var result_1 = result * 365 * 2;
+
+console.log(`You will need ${result_1} to last untill the ripe age of ${age}.`)
+
+
+//zadaca br.4 
+
+var rad = 5;
+
+var result_dia = 2 * rad;
+console.log(`The circumference is ${result_dia}.`) 
+
+var result_area = 3.14 * rad * rad;
+console.log(`The area is ${result_area}.`)
+
+
+//zadaca br.5
+
+var C = 25;
+var result_F = C * 1.8 + 32;
+console.log(`${C} celsius is ${result_F} in farenheit.`);
+
+var F = 50;
+var result_C =  (F - 30) / 2;
+console.log(`${F} farenheit is ${result_C} in celsius.`);
+
+
+
