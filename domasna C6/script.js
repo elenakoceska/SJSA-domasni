2857f6785a4cc2220ae26c3e6283940db3a891ef
elenakoceska <127784516+elenakoceska@users.noreply.github.com>
2023-05-16 19:08:05 +0200

    Add files via upload

---
+++ domasna C6/script.js
@@ -0,0 +1,51 @@
+// zadaca 1
+
+function CountBs(b) {
+    var letterb = b; 
+        for (var i = 0; i <= length; i++) {
+            if (i == "b");
+            
+        };
+        return (`There are ${letterb} in these sports: basketball, baseball, box, badmington`);
+    };
+console.log(CountBs());
+
+
+// zadaca 2
+function LandscapeOrPortrait(width, height) {
+    
+    if(width > height) {
+        return("The dimensions are for Landscape")
+    } else {
+        return("The dimesions are for Portrait")
+    };
+};
+console.log(LandscapeOrPortrait(300, 40));
+
+
+// zadaca 3
+function SpeedLimits(speed) {
+    if (speed <= 50) {
+        return ("Safe driving")
+    } else if (speed > 50, speed < 120) {
+        return (`Speed Limit Crossed. Penalty points:`)
+    } else if (speed > 120) {
+        return ("License Suspended")
+    };
+};
+console.log(SpeedLimits(50));
+
+// zadaca 4
+let car = {
+    brand: "Opel",
+    model: "Corsa",
+    year: 2020,
+    door: 5,
+    color: "black"
+};
+ 
+// function ReturningStrings(car) {
+//     return(`This is string ${car}`);
+
+// };
+// console.log(ReturningStrings(car));
