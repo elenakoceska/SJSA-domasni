ba17f678f760d7f3a4444db4e81c025ca02e0c66
elenakoceska <127784516+elenakoceska@users.noreply.github.com>
2023-05-18 14:59:53 +0200

    Add files via upload

---
+++ domasna C7/script.js
@@ -0,0 +1,9 @@
+// zadaca1
+
+var logo = document.getElementById("logo");
+//logo.img.src='https://s.yimg.com/cv/apiv2/social/images/yahoo_default_logo.png';
+logo.innerHTML = "Yahoo";
+console.dir(logo);
+
+//var button = document.getElementsByName
+
---
+++ domasna C7/script2.js
@@ -0,0 +1,10 @@
+let ul = document.body.style.fontFamily = "Arial, sans-serif";
+document.getElementById("name");
+nickname.innerHTML = "Eli";
+
+document.getElementById("favorites");
+favorites.innerHTML = "books";
+
+document.getElementById("hometown");
+hometown.innerHTML = "Skopje";
+
